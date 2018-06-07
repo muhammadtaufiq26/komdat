3b114ab69b8bcf6c636c0148b66a8c449d8c0101
muhammadtaufiq26 <39506792+muhammadtaufiq26@users.noreply.github.com>
2018-06-07 15:44:48 +0700

    Tugas Final Komdat
    MUHAMMAD TAUFIQ_P3E116020

---
+++ MuhammadTaufiq.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> JARINGAN INTERNET </title>
+</head>
+<body bgcolor="BLUE" text=#FFFFFF>
+<font face="Times New Roman" size="12" leftmargin="200" rightmargin="500"> ARTIKEL TENTANG JARINGAN
+<P Align="Justivy">
+<BR>A.	JARINGAN INTERNET <BR>
+
+<P Align="Justify">Jaringan komputer (jaringan) adalah jaringan telekomunikasi yang memungkinkan antar komputer untuk saling berkomunikasi dengan bertukar data. Tujuan dari jaringan komputer adalah agar dapat mencapai tujuannya, setiap bagian dari jaringan komputer dapat meminta dan memberikan layanan (service).[1] Pihak yang meminta/menerima layanan disebut klien (client) dan yang memberikan/mengirim layanan disebut peladen (server).[1] Desain ini disebut dengan sistem client-server, dan digunakan pada hampir seluruh aplikasi jaringan komputer.[1] Dua buah komputer yang masing-masing memiliki sebuah kartu jaringan, kemudian dihubungkan melalui kabel maupun nirkabel sebagai medium transmisi data, dan terdapat perangkat lunak sistem operasi jaringan akan membentuk sebuah jaringan komputer yang sederhana.[2]: Apabila ingin membuat jaringan komputer yang lebih luas lagi jangkauannya, maka diperlukan peralatan tambahan seperti Hub, Bridge, Switch, Router, Gateway sebagai peralatan interkoneksinya.[2]..<BR></P>
+</body>
+</html>
